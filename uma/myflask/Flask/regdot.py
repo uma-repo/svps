b38b3e6e5ef26557befe33cfaf3b3cdd97b68c62
uma-repo <52880561+uma-repo@users.noreply.github.com>
2022-03-14 14:48:16 +0530

    Update regdot.py

--- uma/myflask/Flask/regdot.py
+++ uma/myflask/Flask/regdot.py
@@ -9,7 +9,7 @@ def home():
         user = request.form['fname']
         print("name is:" +user)
         ##mysql code start
-        conn = mysql.connector.connect(user='root', password='Pass@123', host='127.0.0.1', database='svps')
+        conn = mysql.connector.connect(user='root', password='Pass@123', host='127.0.0.1', database='svps1')
         cursor = conn.cursor()
         insert_stmt = ("INSERT INTO students (studid, Name,Surname,phno,Fromyear,Toyear,Fromclass,Toclass,comments) VALUES (%s, %s, %s, %s, %s,%s,%s,%s,%s)")
         data = ('01', user, 'yerrapragada','9777777','1998','2000','3rd','5th','gfhkdsfuiugh')
