f3160fe3c26e3b0943fcc46982e24d3acc5b9f85
uma <umabhavani8@gmail.com>
2022-04-23 12:03:17 +0530

    cleaned

--- uma/myflask/Flask/backup/pthfun.py
+++
@@ -1,6 +0,0 @@
-def multpl(x,y):
-   return x * y   
-
-num1 = 22
-num2 = 10
-print(multpl(num1,num2));
--- uma/myflask/Flask/backup/retsamfun.py
+++
@@ -1,8 +0,0 @@
-
-def crtlst():
-    mylst = []
-    for each in range(10):
-        mylst.append(each)
-    return mylst
-
-print(crtlst())
